3eb116593559001b2ba8bafc73e8b9ee06ffceb4
bale_lin <bale_lin@163.com>
2018-05-30 11:14:39 +0800

    文档定义关联需求

---
+++ vgrails-scaffolding/src/test/groovy/com/vgrails/demo/StudentCardSpec.groovy
@@ -0,0 +1,18 @@
+package com.vgrails.demo
+
+import grails.testing.gorm.DomainUnitTest
+import spock.lang.Specification
+
+class StudentCardSpec extends Specification implements DomainUnitTest<StudentCard> {
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test something"() {
+        expect:"fix me"
+            true == false
+    }
+}
