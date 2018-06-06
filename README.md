5f2d689d286a188fd32bf9be48e9e50c5daae372
bale_lin <bale_lin@163.com>
2018-06-06 11:30:27 +0800

    提交LOGO

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # VGRAILS企业应用快速开发脚手架
-基于GRAILS3、VUE和ANT.DESIGN的企业级应用快速开发脚手架
+基于GRAILS3、VUE和IVIEW的企业级应用快速开发脚手架
 
 * `vgrails-demp-app`: 演示应用
 * `vgrails-scaffolding`: 脚手架插件
--- vgrails-scaffolding/grails-app/domain/com/vgrails/demo/Organization.groovy
+++ vgrails-scaffolding/grails-app/domain/com/vgrails/demo/Organization.groovy
@@ -7,6 +7,7 @@ import grails.compiler.GrailsCompileStatic
 class Organization {
 
     static transients = ['label']
+
     static m = [
             locale: "组织",
             search: [type: "ajax", fields:['name']],
@@ -16,20 +17,14 @@ class Organization {
             ]
     ]
 
-    String              label
     String              name
     long                level
     Organization        parent
 
     static constraints = {
         parent          attributes:[locale: "上级", associationType: MetaFieldAssociation.one_to_many, associationDomain: 'organization'], nullable: true
-        label           attributes:[locale: "标签"], nullable: true
         name            attributes:[locale: "名称"], size: 2..32, unique: true
         level           attributes:[locale: "层级"], min:0l, max: 32l
-    }
-
-    String getLabel(){
-        return "(${level})${name}"
     }
 
     String toString(){
